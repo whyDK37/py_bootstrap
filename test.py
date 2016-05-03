a1c08a0577caf295db10a4af79bb060c20efc06a
why <why.DK37@gmail.com>
2016-05-03 08:58:37 +0800

    test

--- test.py
+++ test.py
@@ -2,7 +2,9 @@
 # -*- coding: UTF-8 -*-
 
 #将/dd 文件夹下的所有东西复制到/tmp
+import json
 import os,shutil,platform
+from collections import defaultdict
 
 maven_repo_root =   "D:\\mrp"
 maven_onedrive_repo = "D:\\tmp1"
@@ -10,3 +12,23 @@ maven_onedrive_repo = "D:\\tmp1"
 # shutil.copytree(maven_repo_root, maven_onedrive_repo)
 os.popen("ipconfig ").read()
 print(platform.system())
+
+def tree(): return defaultdict(tree)
+
+def dicts(t): return {k: dicts(t[k]) for k in t}
+
+users = tree()
+users['harold']['username'] = 'hrldcpr'
+users['handler']['username'] = 'matthandlersux'
+
+taxonomy = tree()
+taxonomy['Animalia']['Chordata']['Mammalia']['Carnivora']['Felidae']['Felis']['cat']
+taxonomy['Animalia']['Chordata']['Mammalia']['Carnivora']['Felidae']['Panthera']['lion']
+taxonomy['Animalia']['Chordata']['Mammalia']['Carnivora']['Canidae']['Canis']['dog']
+taxonomy['Animalia']['Chordata']['Mammalia']['Carnivora']['Canidae']['Canis']['coyote']
+taxonomy['Plantae']['Solanales']['Solanaceae']['Solanum']['tomato']
+taxonomy['Plantae']['Solanales']['Solanaceae']['Solanum']['potato']
+taxonomy['Plantae']['Solanales']['Convolvulaceae']['Ipomoea']['sweet potato']
+
+print(json.dumps(users))
+print(dicts(taxonomy))
